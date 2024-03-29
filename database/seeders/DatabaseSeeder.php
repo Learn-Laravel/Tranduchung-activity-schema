aad9f9c61883c4792500f2a1155e4b92d6079050
Hung2201 <duchung12a8@gmail.com>
2024-03-29 09:33:25 +0700

    finish

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Item;
+use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(ItemSeeder::class);
+        Item::factory(10) ->create();
+        Product::factory(10) -> create();
     }
 }
---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Item;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $items =[
+        //     ['name' => 'pen', 'price' => 100],
+        //     ['name' => 'book', 'price' => 200],
+        //     ['name' => 'water', 'price' => 44],
+        //     ['name' => 'tv', 'price' => 444],
+        // ];
+        // foreach($items as $item){
+        //     Item::create($item);
+        // }
+    }
+}
